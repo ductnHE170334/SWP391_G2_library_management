036a87c3b35fcb204a90aa153ed8e5515d0d923f
Ductnhe170334 <116861261+AnnoyedPumpkin@users.noreply.github.com>
2024-09-14 15:55:08 +0700

    up base code

---
+++ src/main/java/SWP391_G2/com/example/library_Management/LibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package SWP391_G2.com.example.library_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SWP391_G2/com/example/library_Management/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package SWP391_G2.com.example.library_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
