556100f96b55165a2cfa0cec5f92b6412995c2c3
Ductnhe170334 <116861261+AnnoyedPumpkin@users.noreply.github.com>
2024-09-18 18:52:05 +0700

    build: Create exception folder

---
+++ src/main/java/SWP391_G2/com/example/library_Management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package SWP391_G2.com.example.library_Management.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    ResponseEntity<String> handlingRuntimeException(RuntimeException exception){
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<String> handlingValidation(MethodArgumentNotValidException exception){
+        return  ResponseEntity.badRequest().body(exception.getFieldError().getDefaultMessage());
+    }
+}
