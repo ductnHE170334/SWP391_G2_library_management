1edddc97acd17976f5ee61b21a07330f3e8c5043
AnnoyedPumpkin <snowhead2003@gmail.com>
2024-09-21 22:01:24 +0700

    feat: Add list news function

--- src/main/java/SWP391_G2/com/example/library_Management/Staff/ContentWriter/controller/ContentWriterNewsManagementController.java
+++ src/main/java/SWP391_G2/com/example/library_Management/Staff/ContentWriter/controller/ContentWriterNewsManagementController.java
@@ -1,15 +1,32 @@
 package SWP391_G2.com.example.library_Management.Staff.ContentWriter.controller;
 
+import SWP391_G2.com.example.library_Management.Entity.News;
 import SWP391_G2.com.example.library_Management.Staff.ContentWriter.service.ContentWriterNewsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@Controller
 @RequestMapping("/news")
 public class ContentWriterNewsManagementController {
     @Autowired
     private ContentWriterNewsService contentWriterNewsService;
 
+    //List all news
+    @GetMapping("/list")
+    public String listNews(Model theModel){
+        //Get list from db
+        List<News> news = contentWriterNewsService.getAllNews();
+
+        //Add to spring model
+        theModel.addAttribute("news", news);
+
+        return "Staff/dashboard/News/ManageNews";
+    }
 }
--- src/main/java/SWP391_G2/com/example/library_Management/Staff/ContentWriter/service/ContentWriterNewsService.java
+++ src/main/java/SWP391_G2/com/example/library_Management/Staff/ContentWriter/service/ContentWriterNewsService.java
@@ -1,11 +1,18 @@
 package SWP391_G2.com.example.library_Management.Staff.ContentWriter.service;
 
+import SWP391_G2.com.example.library_Management.Entity.News;
 import SWP391_G2.com.example.library_Management.Staff.ContentWriter.repository.ContentWriterNewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ContentWriterNewsService {
     @Autowired
     private ContentWriterNewsRepository contentWriterNewsRepository;
+
+    public List<News> getAllNews(){
+        return contentWriterNewsRepository.findAll();
+    }
 }
