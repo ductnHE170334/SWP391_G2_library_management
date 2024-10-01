f877c76dae4ed220b379b3171ec357fa9c118c45
Nguyen Quang Huy <nguyenhuyquang1096@gmail.com>
2024-10-01 16:21:05 +0700

    add registerDTO and customerRepo

--- src/main/java/SWP391_G2/com/example/library_Management/Customer/dto/request/CustomerRegisterRequest.java
+++ src/main/java/SWP391_G2/com/example/library_Management/Customer/dto/request/CustomerRegisterRequest.java
@@ -9,6 +9,50 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CustomerRegisterRequest {
-    String lastName;
-    String firstName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+
+    // Getters và Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
--- src/main/java/SWP391_G2/com/example/library_Management/Customer/repository/CustomerRepository.java
+++ src/main/java/SWP391_G2/com/example/library_Management/Customer/repository/CustomerRepository.java
@@ -2,7 +2,13 @@ package SWP391_G2.com.example.library_Management.Customer.repository;
 
 import SWP391_G2.com.example.library_Management.Entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CustomerRepository extends JpaRepository<Customer, String> {
+import java.util.Optional;
 
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findOneByEmailAndPassword(String email, String password);
+    Customer findByEmail(String email);
 }
