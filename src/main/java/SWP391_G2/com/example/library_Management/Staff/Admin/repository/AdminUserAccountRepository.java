168437e39cfd0512ab23ef58d84279f4238c6960
Hà Huy Hoàng <hoanghhhe176425@fpt.edu.vn>
2024-10-23 23:18:44 +0700

    Update Manage User for Admin

--- src/main/java/SWP391_G2/com/example/library_Management/Staff/Admin/repository/AdminUserAccountRepository.java
+++ src/main/java/SWP391_G2/com/example/library_Management/Staff/Admin/repository/AdminUserAccountRepository.java
@@ -1,13 +1,13 @@
 package SWP391_G2.com.example.library_Management.Staff.Admin.repository;
 
-import SWP391_G2.com.example.library_Management.Entity.Staff;
+import SWP391_G2.com.example.library_Management.Entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminStaffAccountRepository extends JpaRepository<Staff, Integer> {
-    Page<Staff> findByLastNameContaining(String lastName, Pageable pageable);
+public interface AdminUserAccountRepository extends JpaRepository<User, Integer> {
+    Page<User> findByLastNameContaining(String lastName, Pageable pageable);
 }
 
