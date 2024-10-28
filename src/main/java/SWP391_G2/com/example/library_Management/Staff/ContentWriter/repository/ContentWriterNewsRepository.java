8e46fd3904d1660e8d8b5194a7306af62c46076b
Nguyen Quang Huy <nguyenhuyquang1096@gmail.com>
2024-10-28 22:41:03 +0700

    add and modify repo, controller, service to staff role

--- src/main/java/SWP391_G2/com/example/library_Management/Staff/ContentWriter/repository/ContentWriterNewsRepository.java
+++ src/main/java/SWP391_G2/com/example/library_Management/Staff/ContentWriter/repository/ContentWriterNewsRepository.java
@@ -1,16 +1,16 @@
-package SWP391_G2.com.example.library_Management.Staff.ContentWriter.repository;
-
-import SWP391_G2.com.example.library_Management.Entity.News;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-
-public interface ContentWriterNewsRepository extends JpaRepository<News, Integer> {
-
-    @Query("SELECT n FROM News n WHERE n.title like %?1%")
-    Page<News> findNewsByTitle(String title, Pageable pageable);
-}
+//package SWP391_G2.com.example.library_Management.Staff.ContentWriter.repository;
+//
+//import SWP391_G2.com.example.library_Management.Entity.News;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//
+//import java.util.List;
+//
+//public interface ContentWriterNewsRepository extends JpaRepository<News, Integer> {
+//
+//    @Query("SELECT n FROM News n WHERE n.title like %?1%")
+//    Page<News> findNewsByTitle(String title, Pageable pageable);
+//}
