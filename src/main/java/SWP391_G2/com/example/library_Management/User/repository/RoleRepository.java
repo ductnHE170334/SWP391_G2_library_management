ac5ae31b373f2c2ad77372af3d8e9d73d589d89d
ductnHE170334 <ductnhe170334@fpt.edu.vn>
2024-10-28 23:09:10 +0700

    Revert "Merge branch 'huynqhe180360' into test"
    This reverts commit 6b81ea1f65240d37e708d69a6586ebdf2529b677, reversing
    changes made to af684cbab088ddf327db9c8d13c5b669496022c6.

--- src/main/java/SWP391_G2/com/example/library_Management/User/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package SWP391_G2.com.example.library_Management.User.repository;
-
-import SWP391_G2.com.example.library_Management.Entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Role findByName(String name);
-}
