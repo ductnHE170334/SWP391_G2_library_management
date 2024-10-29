c18b941ce106ed95c21b3ef94ea79d6e717f9b00
Hà Huy Hoàng <hoanghhhe176425@fpt.edu.vn>
2024-10-29 19:53:12 +0700

    Update Category for admin AdminCategoryManagementController.java

--- src/main/java/SWP391_G2/com/example/library_Management/Staff/Admin/controller/AdminCategoryManagementController.java
+++ src/main/java/SWP391_G2/com/example/library_Management/Staff/Admin/controller/AdminCategoryManagementController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/category")
+@RequestMapping("admin/dashboard/category")
 public class AdminCategoryManagementController {
 
     private final AdminCategoryService adminCategoryService;
