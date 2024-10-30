359b55be03a94e22de5d8cb8a27adc242a967105
Nguyen Minh Quan K17_HL <quannmhe170597@fpt.edu.vn>
2024-10-30 22:28:38 +0700

    add the book item list in the book detail controller

--- src/main/java/SWP391_G2/com/example/library_Management/Staff/Librarian/controller/LibrarianBookDetailController.java
+++ src/main/java/SWP391_G2/com/example/library_Management/Staff/Librarian/controller/LibrarianBookDetailController.java
@@ -1,6 +1,7 @@
 package SWP391_G2.com.example.library_Management.Staff.Librarian.controller;
 
 import SWP391_G2.com.example.library_Management.Entity.Book;
+import SWP391_G2.com.example.library_Management.Entity.Book_item;
 import SWP391_G2.com.example.library_Management.Staff.Librarian.service.LibrarianBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/librarian/bookDetail")
@@ -17,7 +20,10 @@ public class LibrarianBookDetailController {
     @GetMapping("/{bookId}")
     public String getBookDetails(@PathVariable int bookId, Model model) {
         Book book = librarianBookService.getBookById(bookId);
+        List<Book_item> bookItems = librarianBookService.getBookItemsByBookId(bookId);
         model.addAttribute("book", book);
+        model.addAttribute("bookItems", bookItems);
         return "Staff/dashboard/Borrow_request/BookDetail";
     }
+
 }
