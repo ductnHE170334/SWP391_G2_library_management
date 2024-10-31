8b65112991b7337b18f4dd17c9211d4241eafc52
Hà Huy Hoàng <hoanghhhe176425@fpt.edu.vn>
2024-10-31 16:33:20 +0700

    Screen staff detail for admin AdminStaffDetailRepository.java

---
+++ src/main/java/SWP391_G2/com/example/library_Management/Staff/Admin/repository/AdminStaffDetailRepository.java
@@ -0,0 +1,7 @@
+package SWP391_G2.com.example.library_Management.Staff.Admin.repository;
+
+import SWP391_G2.com.example.library_Management.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminStaffDetailRepository extends JpaRepository<User, Long> {
+}
