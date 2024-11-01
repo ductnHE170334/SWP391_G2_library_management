f2d48b6ce6368c6a16293f090627fb4adb784957
Nguyen Minh Quan K17_HL <quannmhe170597@fpt.edu.vn>
2024-11-01 14:52:01 +0700

    add the logout function

--- src/main/java/SWP391_G2/com/example/library_Management/User/controller/UserLogoutController.java
+++ src/main/java/SWP391_G2/com/example/library_Management/User/controller/UserLogoutController.java
@@ -1,10 +1,17 @@
 package SWP391_G2.com.example.library_Management.User.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/logout")
 public class UserLogoutController {
 
+    @GetMapping
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/home/list"; // or "redirect:/login" if you prefer
+    }
 }
