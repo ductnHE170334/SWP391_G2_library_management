1586ce3e553f9b21a39be162a329933b1afe11e4
Nguyen Minh Quan K17_HL <quannmhe170597@fpt.edu.vn>
2024-11-01 14:49:49 +0700

    add loginUser function

--- src/main/java/SWP391_G2/com/example/library_Management/User/service/UserLoginService.java
+++ src/main/java/SWP391_G2/com/example/library_Management/User/service/UserLoginService.java
@@ -1,11 +1,18 @@
 package SWP391_G2.com.example.library_Management.User.service;
 
-import SWP391_G2.com.example.library_Management.User.repository.UserRepository;
+import SWP391_G2.com.example.library_Management.Customer.repository.UserRepository;
+import SWP391_G2.com.example.library_Management.Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserLoginService {
+
     @Autowired
     private UserRepository userRepository;
+
+    // Authenticate the user by email and password
+    public User loginUser(String email, String password) {
+        return userRepository.findByEmailAndPassword(email, password);
+    }
 }
