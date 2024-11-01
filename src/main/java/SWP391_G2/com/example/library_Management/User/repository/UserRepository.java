41ae6a5c353741fd670cb0ddc5a802ab45e539da
Nguyen Minh Quan K17_HL <quannmhe170597@fpt.edu.vn>
2024-11-01 14:47:03 +0700

    add structure for login register

--- src/main/java/SWP391_G2/com/example/library_Management/User/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package SWP391_G2.com.example.library_Management.User.repository;
-import SWP391_G2.com.example.library_Management.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
