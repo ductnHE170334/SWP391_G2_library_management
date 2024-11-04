b263d5355b9652368b7cffb3d922d02ac1619b26
Nguyen Minh Quan K17_HL <quannmhe170597@fpt.edu.vn>
2024-11-04 22:08:53 +0700

    create viewAddApplication function

--- src/main/java/SWP391_G2/com/example/library_Management/Customer/controller/CustomerApplicationController.java
+++ src/main/java/SWP391_G2/com/example/library_Management/Customer/controller/CustomerApplicationController.java
@@ -30,5 +30,26 @@ public class CustomerApplicationController {
 
         return "Customer/Application/ListApplication";
     }
+    @GetMapping
+    public String addApplicationView(HttpSession session, Model model) {
+        Object userIdObj = session.getAttribute("userId");
+        Long userId = null;
+
+
+        if (userIdObj instanceof Long) {
+            userId = (Long) userIdObj;
+        } else if (userIdObj instanceof Integer) {
+            userId = Long.valueOf((Integer) userIdObj);
+        }
+
+        if (userId == null) {
+            // Xử lý trường hợp userId không được thiết lập trong session
+            model.addAttribute("message", "User not logged in.");
+            return "redirect:/login"; // Chuyển hướng đến trang đăng nhập hoặc trang phù hợp
+        }
+        model.addAttribute("userId", userId);
+
+        return "Customer/Application/AddApplication";
+    }
 }
 
