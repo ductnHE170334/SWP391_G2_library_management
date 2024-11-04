add03752c1d0f318f0a0b0d3a01105ba6c884503
Nguyen Minh Quan K17_HL <quannmhe170597@fpt.edu.vn>
2024-11-05 06:43:42 +0700

    add application service for customer

---
+++ src/main/java/SWP391_G2/com/example/library_Management/Customer/service/CustomerApplicationService.java
@@ -0,0 +1,26 @@
+package SWP391_G2.com.example.library_Management.Customer.service;
+
+import SWP391_G2.com.example.library_Management.Customer.repository.CustomerApplicationRepository;
+import SWP391_G2.com.example.library_Management.Customer.repository.CustomerApplicationTypeRepository;
+import SWP391_G2.com.example.library_Management.Entity.Application;
+import SWP391_G2.com.example.library_Management.Entity.Application_Type;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerApplicationService {
+    @Autowired
+    CustomerApplicationRepository customerApplicationRepository;
+    @Autowired
+    private CustomerApplicationTypeRepository customerApplicationTypeRepository;
+    public List<Application> getApplicationsByUserId(Long userId) {
+        return customerApplicationRepository.findByUser_Id(userId);
+    }
+
+
+    public List<Application_Type> getAllApplicationTypes() {
+        return customerApplicationTypeRepository.findAll();
+    }
+}
