4cdcc9711f05112bcbac53acff4303f123434175
Nguyen Minh Quan K17_HL <quannmhe170597@fpt.edu.vn>
2024-11-07 11:15:38 +0700

    add function viewApplication detail and update application

--- src/main/java/SWP391_G2/com/example/library_Management/Staff/Librarian/controller/LibrarianApplicationController.java
+++ src/main/java/SWP391_G2/com/example/library_Management/Staff/Librarian/controller/LibrarianApplicationController.java
@@ -1,14 +1,14 @@
 package SWP391_G2.com.example.library_Management.Staff.Librarian.controller;
 
 import SWP391_G2.com.example.library_Management.Entity.Application;
+import SWP391_G2.com.example.library_Management.Entity.Application_Type;
 import SWP391_G2.com.example.library_Management.Staff.Librarian.service.LibrarianApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -28,5 +28,33 @@ public class LibrarianApplicationController {
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", applicationPage.getTotalPages());
         return "Staff/dashboard/Application/listApplication";
+    }
+    @GetMapping("/viewApplicationDetail/{id}")
+    public String viewApplicationDetail(@PathVariable("id") Integer id, Model model) {
+        Application application = librarianApplicationService.getApplicationById(id);
+        List<Application_Type> applicationTypes = librarianApplicationService.getAllApplicationTypes();
+
+        System.out.println("======TEST DATA=====");
+        System.out.println(application.getApplication_type().getApplication_type());
+        model.addAttribute("applicationTypes", applicationTypes);
+        model.addAttribute("application", application);
+        return "Staff/dashboard/Application/viewApplicationDetail";
+    }
+    @PostMapping("/save")
+    public String saveApplication(@ModelAttribute("application") Application application,
+
+                                  RedirectAttributes redirectAttributes) {
+
+
+
+
+
+        // Save the updated application
+        librarianApplicationService.update(application);
+
+        // Add success message
+        redirectAttributes.addFlashAttribute("successMessage", "Application has been saved successfully.");
+
+        return "redirect:/librarian/application/list";
     }
 }
